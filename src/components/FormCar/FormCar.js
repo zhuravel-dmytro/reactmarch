301b0b88bf81b127ec9ef31ce247d7146f7e08ee
Dimas <zhuravel16@gmail.com>
2022-07-09 01:55:51 +0300

    form треба доробити

--- src/components/FormCar/FormCar.js
+++ src/components/FormCar/FormCar.js
@@ -1,13 +1,23 @@
 import React from 'react';
+
 import {useForm} from "react-hook-form";
+
+import css from './FormCar.css'
 
 const FormCar = () => {
     const {register,handleSubmit,reset,setValue,formState:{errors}} = useForm();
 
+    function submit() {
+
+    }
+
     return (
-        <div>
-            <form action="">
-                <input type="text" />
+        <div className={'formContainer'}>
+            <form onSubmit={handleSubmit(submit)}>
+                <input type="text" placeholder={'model'} {...register('model',{})}/>
+                <input type="number" placeholder={'price'} {...register('price',{})}/>
+                <input type="number" placeholder={'year'} {...register('year',{})}/>
+                <button>done</button>
             </form>
         </div>
     );
