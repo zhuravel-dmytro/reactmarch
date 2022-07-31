a7fa3bd10e5deea6974b0d1de36bb7b5c7ea7f45
Dimas <zhuravel16@gmail.com>
2022-07-31 20:56:09 +0300

    done

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,57 +1,63 @@
 // 1) створити інтерфейс на основі цього объекта:
-type Iinformation =
-{
+interface IInformation {
     "mission_name": string,
     "launch_date_local": number,
     "launch_site": {
-    "site_name_long": string
-},
-    "links": {
-    "article_link": null,
-        "video_link": string
-},
-    "rocket": {
-    "rocket_name": string,
-        "first_stage": {
-        "cores": [
-            {
-                "flight": number,
-                "core": {
-                    "reuse_count": number,
-                    "status": string
-                }
-            }
-        ]
+        "site_name_long": string
     },
-    "second_stage": {
-        "payloads": [
-            {
-                "payload_type": string,
-                "payload_mass_kg": number,
-                "payload_mass_lbs": number
-            }
-        ]
+    "links": {
+        "article_link": null,
+        "video_link": string
+    },
+    "rocket": {
+        "rocket_name": string,
+        "first_stage": {
+            "cores": [
+                {
+                    "flight": number,
+                    "core": {
+                        "reuse_count": number,
+                        "status": boolean
+                    }
+                }
+            ]
+        },
+        "second_stage": {
+            "payloads": [
+                {
+                    "payload_type": string,
+                    "payload_mass_kg": number,
+                    "payload_mass_lbs": number
+                }
+            ]
+        }
     }
 }
-}
+
 
 // ---------------------------------------------------------------------------
 // 2) протипізувати функції:
+interface IUser {
+    name:string,
+    age:number,
+    gender:string
+}
 
-    const user = {
+    const user:IUser = {
         name:"Max",
         age:18,
         gender:'male'
     }
 
-function sum(a,b){
+function sum(a:number,b:number):number{
     return a+b
 }
-function showSum(a,b){
+
+function showSum(a:number,b:number):number{
     console.log(a + b);
 }
 
-function incAge(someUser, inc){
+function incAge(someUser:IUser, inc:number):IUser{
     someUser.age+=inc
     return someUser
 }
