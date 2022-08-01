c87a7a54cf755a65bf483d6b272ab7e2b009a887
Dimas <zhuravel16@gmail.com>
2022-08-01 19:58:47 +0300

    виправив

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,35 +1,35 @@
 // 1) створити інтерфейс на основі цього объекта:
+interface ICore {
+    flight: number,
+    core: {
+        reuse_count: number,
+        status: boolean
+    }
+}
+
+interface IPayloads {
+    payload_type: string,
+    payload_mass_kg: number,
+    payload_mass_lbs: number
+}
+
 interface IInformation {
-    "mission_name": string,
-    "launch_date_local": number,
-    "launch_site": {
-        "site_name_long": string
+    mission_name: string,
+    launch_date_local: number,
+    launch_site: {
+        site_name_long: string
     },
-    "links": {
-        "article_link": null,
-        "video_link": string
+    links: {
+        article_link: null,
+        video_link: string
     },
-    "rocket": {
-        "rocket_name": string,
-        "first_stage": {
-            "cores": [
-                {
-                    "flight": number,
-                    "core": {
-                        "reuse_count": number,
-                        "status": boolean
-                    }
-                }
-            ]
+    rocket: {
+        rocket_name: string,
+        first_stage: {
+            cores: ICore[]
         },
-        "second_stage": {
-            "payloads": [
-                {
-                    "payload_type": string,
-                    "payload_mass_kg": number,
-                    "payload_mass_lbs": number
-                }
-            ]
+        second_stage: {
+            payloads: IPayloads[]
         }
     }
 }
